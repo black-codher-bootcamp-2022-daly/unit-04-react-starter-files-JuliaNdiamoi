aaf3b128dd2ec7a04219e64d2e5bcb8125d3a05f
Julia N'diamoi <ndiamoij@gmail.com>
2022-12-05 17:39:24 +0000

    bookcase

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import Book from './components/Book';
-import data from './models/books.json';
+import React, { useState } from "react";
+
+import Book from "./components/Book";
+import data from "./models/books.json";
 
 function App() {
-  const books = data;
-  return <Book/>;
+  // const books = data
+  const [books] = useState(data);
+
+  return <div>
+  <h1>Tobia's library</h1>
+  {books.map((book) => <Book key={book.id} book={book} />)}
+  </div>
 }
 
 export default App;
-
